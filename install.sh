65dd93133f0f2f268ecdd46aaa653a9b15af9eb7
Florian Wartner <florian@wartner.io>
2021-06-15 11:35:45 +0200

    Changes

--- install.sh
+++ install.sh
@@ -16,6 +16,9 @@ brew bundle
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+ln -s $HOME/.dotfiles/config/.mackup.cfg $HOME/.mackup.cfg
 
 ./ssh.sh
